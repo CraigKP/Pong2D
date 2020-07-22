94c2015dc0cb35bfe04a55101fb9d040b4a50eff
Pinto, Craig Kavan <m1cpinto@zeiss.com>
2020-07-22 19:54:04 +0200

    Push Pong2D to Master

---
+++ Pong 2D/Assets/Ball.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private Vector2 direction;
+    private float radius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.direction = Vector2.one.normalized; // direction is (1, 1), so magnitude is root(2), which isn't good. normalized means magnitude is also 1.
+        this.radius = transform.localScale.x / 2; // diameter / 2
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(this.direction * this.speed * Time.deltaTime); // this '* Time.deltaTime' makes input time independent. So if there is lag, it wont slow down.
+
+        // restrict movement
+        // If paddle is too high up, and hits the wall, then invert the direction
+        if (transform.position.y < (GameManager.bottomLeft.y + this.radius) && this.direction.y < 0)
+        {
+            // 'this.direction.y < 0' because moving down is indicated by negative move
+            // '+ this.radius' because we want the bottom to not go below the minimum limit
+            this.direction.y = -this.direction.y;
+        }
+
+        // If paddle is too low down, and user continues to move down, stop movement (set move = 0)
+        if (transform.position.y > (GameManager.topRight.y - this.radius) && this.direction.y > 0)
+        {
+            // 'this.direction.y > 0' because moving up is indicated by positive move
+            // '+ this.radius' because we want the bottom to not go below the minimum limit
+            this.direction.y = -this.direction.y;
+        }
+
+        // Game Over
+        // If paddle hits left wall, right player wins
+        if (transform.position.x < (GameManager.bottomLeft.x + this.radius) && this.direction.x < 0)
+        {
+            Debug.Log("Right Player Wins!!");
+
+            // Freeze time and end game
+            Time.timeScale = 0;
+            enabled = false; // Stops the script. I.e.: update doesn't happen.
+        }
+
+        // If paddle hits right wall, right player wins
+        if (transform.position.x > (GameManager.topRight.x - this.radius) && this.direction.x > 0)
+        {
+            Debug.Log("left Player Wins!!");
+
+            // Freeze time and end game
+            Time.timeScale = 0;
+            enabled = false; // Stops the script. I.e.: update doesn't happen.
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Paddle")
+        {
+            bool isRight = other.GetComponent<Paddle>().isRight;
+
+            // this.direction.x > 0 means we are moving to the right side
+            // is hitting right paddle, change direction
+            if (isRight && this.direction.x > 0)
+            {
+                this.direction.x = -this.direction.x;
+            }
+
+            // this.direction.x < 0 means we are moving to the left side
+            // is hitting left paddle, change direction
+            if (!isRight && this.direction.x < 0)
+            {
+                this.direction.x = -this.direction.x;
+            }
+        }
+    }
+}
---
+++ Pong 2D/Assets/Paddle.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float height;
+
+    private string input;
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+        //speed = 5f; // some constant value
+    }
+
+    public void Init(bool isRightPaddle)
+    {
+        isRight = isRightPaddle; 
+
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle)
+        {
+            // if it is a right paddle place on right of the screen
+            // however this moves the center of the paddle to the right.
+            pos = new Vector2(GameManager.topRight.x, 0);
+
+            // we want the right edge to be at the right, so we remove the right hand side of the position vector with the width of x (x axis is right/left plane)
+            pos -= Vector2.right * transform.localScale.x;
+
+            this.input = "PaddleRight";
+        }
+        else
+        {
+            // else it is a left paddle place on left of the screen
+            // however this moves the center of the paddle to the left.
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+
+            // we want the left edge to be at the left, so we add the left hand side of the position vector with the width of x (x axis is right/left plane)
+            pos += Vector2.right * transform.localScale.x;
+
+            this.input = "PaddleLeft";
+        }
+
+        // Update this paddles position
+        // Every game object has a transform, here we set transform to the position relative to the screen.
+        transform.position = pos;
+
+        transform.name = this.input;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // To move the paddle
+
+        // GetAxis is a number between -1 and 1 (-1 for down, 1 for up)
+        float move = Input.GetAxis(this.input) * Time.deltaTime * this.speed; // this '* Time.deltaTime' makes input time independent. So if there is lag, it wont slow down.
+
+        // restrict movement
+        // If paddle is too high up, and user continues to move up, stop movement (set move = 0)
+        if (transform.position.y < (GameManager.bottomLeft.y + this.height / 2) && move < 0)
+        {
+            // 'move < 0' because moving down is indicated by negative move
+            // '+ this.height / 2' because we want the bottom to not go below the minimum limit
+            move = 0;
+        }
+        
+        // If paddle is too low down, and user continues to move down, stop movement (set move = 0)
+        if (transform.position.y > (GameManager.topRight.y - this.height / 2) && move > 0)
+        {
+            // 'move < 0' because moving up is indicated by positive move
+            // '+ this.height / 2' because we want the bottom to not go below the minimum limit
+            move = 0;
+        }
+
+        transform.Translate(move * Vector2.up);
+    }
+}
